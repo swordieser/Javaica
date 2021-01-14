0eb167d3971c7c2a7dec7b842b68be1779415149
Serj <swordie.ser@gmail.com>
2021-01-14 03:36:36 +0300

    version 1.2 needs to be tested

--- src/Whatever.java
+++ src/Whatever.java
@@ -56,7 +56,7 @@ public class Whatever {
                     });
                     break;
             }
-            System.out.println("Whatever");
+
             command = reader.readLine().split(" ");
         }
     }
