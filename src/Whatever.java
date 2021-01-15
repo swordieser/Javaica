02d5a69e0eba96506092b769fada6920b004a9b2
Serj <swordie.ser@gmail.com>
2021-01-15 14:03:14 +0300

    version 1.4

--- src/Whatever.java
+++ src/Whatever.java
@@ -6,20 +6,27 @@ public class Whatever {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<String> tasks = new ArrayList<>();
         String[] command = reader.readLine().split(" ");
-
-        while (!command[0].equals("exit")) {
-            switch (command[0]) {
+        ArrayList<String> add_tasks = new ArrayList<>();
+        String[] com = command;
+        while (!com[0].equals("exit")) {
+            if (add_tasks.size() != 0) {
+                com = add_tasks.get(0).split(" ");
+                add_tasks.remove(0);
+            } else {
+                com = command;
+            }
+            switch (com[0]) {
                 case "add":
                     int i = 0;
                     for (String string : tasks) {
-                        if (command[1].equals(string)) {
+                        if (com[1].equals(string)) {
                             i++;
                         }
                     }
                     if (i != 0) {
-                        tasks.add(command[1] + "(" + Integer.toString(i) + ")");
+                        tasks.add(com[1] + "(" + Integer.toString(i) + ")");
                     } else {
-                        tasks.add(command[1]);
+                        tasks.add(com[1]);
                     }
                     break;
                 case "all":
@@ -30,34 +37,36 @@ public class Whatever {
                     System.out.println(tasks.get(j) + ".");
                     break;
                 case "delete":
-                    tasks.remove(command[1]);
+                    tasks.remove(com[1]);
                     break;
                 case "save":
-                    String savename = command[1];
+                    String savename = com[1];
                     FileWriter fw = new FileWriter(savename);
                     tasks.forEach((n) -> {
                         try {
                             fw.write(n);
                         } catch (IOException e) {
                             e.printStackTrace();
+                            System.out.println("Save error");
                         }
                     });
-
                     fw.close();
                     break;
                 case "load":
-                    String loadname = command[1];
+                    String loadname = com[1];
+
                     try (BufferedReader br = new BufferedReader(new FileReader(loadname))) {
                         String line;
                         while ((line = br.readLine()) != null) {
-                            tasks.add(line);
+                            add_tasks.add(line);
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
+                        System.out.println("Load error");
                     }
                     break;
                 case "complete":
-                    String task = command[1];
+                    String task = com[1];
                     tasks.remove(task);
                     task += " - completed";
                     tasks.add(task);
@@ -70,8 +79,9 @@ public class Whatever {
                     });
                     break;
             }
-
-            command = reader.readLine().split(" ");
+            if (add_tasks.size() == 0) {
+                command = reader.readLine().split(" ");
+            }
         }
     }
 }
